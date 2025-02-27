88d43fc7ead93455c31c9efd946814809025e8e4
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2025-02-27 10:27:46 +0000

    first commit

---
+++ src/main/java/com/codev13/springkeycloakddd/SpringKeycloakDddApplication.java
@@ -0,0 +1,13 @@
+package com.codev13.springkeycloakddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeycloakDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKeycloakDddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codev13/springkeycloakddd/SpringKeycloakDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codev13.springkeycloakddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeycloakDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
